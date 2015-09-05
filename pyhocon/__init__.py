59c601352a1fa94326414e76d905a0e344cb09d0
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2015-09-04 20:15:12 -0400

    added import into __init__ for backward compatibility

--- pyhocon/__init__.py
+++ pyhocon/__init__.py
@@ -1,2 +1,5 @@
 from .config_parser import ConfigParser, ConfigFactory, ConfigSubstitutionException  # noqa
+from .config_tree import ConfigTree, ConfigList, UndefinedKey, \
+    ConfigInclude, ConfigSubstitution, ConfigUnquotedString, ConfigValues, \
+    ConfigMissingException, ConfigException, ConfigWrongTypeException  # noqa
 from .tool import HOCONConverter  # noqa
