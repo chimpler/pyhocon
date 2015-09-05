12dc9fac819085f0db76d3171d79add0ba1d1145
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2015-09-04 20:18:22 -0400

    flake8 fix

--- pyhocon/__init__.py
+++ pyhocon/__init__.py
@@ -1,5 +1,5 @@
 from .config_parser import ConfigParser, ConfigFactory, ConfigSubstitutionException  # noqa
-from .config_tree import ConfigTree, ConfigList, UndefinedKey, \
-    ConfigInclude, ConfigSubstitution, ConfigUnquotedString, ConfigValues, \
-    ConfigMissingException, ConfigException, ConfigWrongTypeException  # noqa
+from .config_tree import ConfigTree, ConfigList, UndefinedKey  # noqa
+from .config_tree import ConfigInclude, ConfigSubstitution, ConfigUnquotedString, ConfigValues  # noqa
+from .config_tree import ConfigMissingException, ConfigException, ConfigWrongTypeException  # noqa
 from .tool import HOCONConverter  # noqa
