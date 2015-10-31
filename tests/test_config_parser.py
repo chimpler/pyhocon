ee939f75a88ec3c2b162bc6bc68ff2cd4e050d89
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2015-10-30 20:40:26 -0400

    fixed parsing of last line when no eol

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -515,19 +515,25 @@ class TestConfigParser(object):
             'cluster-size': 6
         }
 
-    def test_dos_chars(self):
-        config = ConfigFactory.parse_string(
-            """
-            app.heap_size = 128\r
-            app.java_opts = [\r
-                -Xms${app.heap_size}m\r
-                -Xmx${app.heap_size}m\r
-            ]\r
-            foo = bar\r
-            """
-        )
-
+    def test_dos_chars_with_unquoted_string_noeol(self):
+        config = ConfigFactory.parse_string("foo = bar")
         assert config['foo'] == 'bar'
+
+    def test_dos_chars_with_quoted_string_noeol(self):
+        config = ConfigFactory.parse_string('foo = "5"')
+        assert config['foo'] == '5'
+
+    def test_dos_chars_with_triple_quoted_string_noeol(self):
+        config = ConfigFactory.parse_string('foo = """5"""')
+        assert config['foo'] == '5'
+
+    def test_dos_chars_with_int_noeol(self):
+        config = ConfigFactory.parse_string("foo = 5")
+        assert config['foo'] == 5
+
+    def test_dos_chars_with_float_noeol(self):
+        config = ConfigFactory.parse_string("foo = 5.0")
+        assert config['foo'] == 5.0
 
     def test_list_substitutions(self):
         config = ConfigFactory.parse_string(
