35f4ce3f02eaedbac6393253bb1771813d194c79
gabi <gabis@precog.co>
2022-03-19 21:17:18 +0200

    added python 310 to tox list. test refactoring and reformatting

--- tests/test_periods.py
+++ tests/test_periods.py
@@ -61,6 +61,8 @@ def test_parse_string_with_duration(data_set):
 
 try:
     from dateutil.relativedelta import relativedelta
+
+
     @pytest.mark.parametrize('data_set', [
         ('1 months', relativedelta(months=1)),
         ('1months', relativedelta(months=1)),
@@ -79,6 +81,17 @@ try:
         parsed = parse_period(data_set[0])
 
         assert parsed == data_set[1]
+
+
+    def test_format_relativedelta():
+
+        for time_delta, expected_result in ((relativedelta(seconds=0), '0 seconds'),
+                                            (relativedelta(hours=0), '0 seconds'),
+                                            (relativedelta(days=5), '5 days'),
+                                            (relativedelta(weeks=3), '21 days'),
+                                            (relativedelta(hours=2), '2 hours'),
+                                            (relativedelta(minutes=43), '43 minutes'),):
+            assert expected_result == timedelta_to_hocon(time_delta)
 except ImportError:
     pass
 
@@ -88,19 +101,4 @@ def test_format_time_delta():
                                         (timedelta(days=5), '5 days'),
                                         (timedelta(seconds=51), '51 seconds'),
                                         (timedelta(microseconds=786), '786 microseconds')):
-        assert expected_result == timedelta_to_hocon(time_delta)
-
-
-def test_format_relativedelta():
-    try:
-        from dateutil.relativedelta import relativedelta
-    except ImportError:
-        return
-
-    for time_delta, expected_result in ((relativedelta(seconds=0), '0 seconds'),
-                                        (relativedelta(hours=0), '0 seconds'),
-                                        (relativedelta(days=5), '5 days'),
-                                        (relativedelta(weeks=3), '21 days'),
-                                        (relativedelta(hours=2), '2 hours'),
-                                        (relativedelta(minutes=43), '43 minutes'),):
         assert expected_result == timedelta_to_hocon(time_delta)
