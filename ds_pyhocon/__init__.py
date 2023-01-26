b2107a43fa30150ff963bc14ef61b04731ad07e1
Arthur Faisman <arthur.faisman@stackadapt.com>
2023-01-26 14:48:54 -0500

    Revert "add bumpversion"
    This reverts commit fdc5f96e34491ff88ea9e564356d49b9e866c4aa.

--- ds_pyhocon/__init__.py
+++ ds_pyhocon/__init__.py
@@ -1,7 +1,3 @@
-__author__ = """Arthur Faisman"""
-__email__ = "arthur.faisman@stackadapt.com"
-__version__ = "1.0.01"
-
 from .config_parser import ConfigParser, ConfigFactory, ConfigSubstitutionException  # noqa
 from .config_tree import ConfigTree, ConfigList, UndefinedKey  # noqa
 from .config_tree import ConfigInclude, ConfigSubstitution, ConfigUnquotedString, ConfigValues  # noqa
